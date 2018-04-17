b3395f447b4862053ac978301219e2f3c3777bfb
Colin Tindle <ampersand@underscore.space>
2018-04-17 14:24:44 +0100

    created a bus class

---
+++ bus.rb
@@ -0,0 +1,12 @@
+class Bus
+
+  attr_reader :route_number, :destination
+
+  def initialize(route_number, destination)
+    @route_number = route_number
+    @destination = destination
+  end
+
+
+  
+end 
